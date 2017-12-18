6a1c45a4de67444331d5861ecb296983efd602bd
Dumming <vrednokot@gmail.com>
2017-12-18 21:58:45 +0300

    fix effect-filters
    but now need to fix effect-controls

--- js/change-filter.js
+++ js/change-filter.js
@@ -1,9 +1,29 @@
 'use strict';
 
 (function () {
-  var uploadEffectsControls = document.querySelector('.upload-effect-controls');
-  var imagePreview = document.querySelector('.effect-image-preview');
+  var pictureElement = document.querySelector('.upload-form-preview img');
+  var uploadEffectControl = document.querySelector('.upload-effect-controls');
 
-  window.initializeFilters(uploadEffectsControls, imagePreview);
+  var uploadEffectLevel = document.querySelector('.upload-effect-level');
+  var effectLevelPin = document.querySelector('.upload-effect-level-pin');
+  var effectLevelVal = document.querySelector('.upload-effect-level-val');
+
+  var applyFilter = function (newFilter, oldFilter) {
+    pictureElement.classList.remove('effect-' + oldFilter);
+    pictureElement.classList.add('effect-' + newFilter);
+    changeVisibility(pictureElement, uploadEffectLevel);
+    resetEffectValue();
+  };
+
+  window.initializeFilters(uploadEffectControl, applyFilter);
+
+  var resetEffectValue = function () {
+    effectLevelPin.style.left = '100%';
+    effectLevelVal.style.width = '100%';
+  };
+
+  var changeVisibility = function (element, target) {
+    return target.value !== 'none' ? target.classList.remove('hidden') : target.classList.add('hidden');
+  };
 
 })();
--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -2,34 +2,12 @@
 
 (function () {
   window.initializeFilters = function (filterElement, callback) {
-    var uploadEffectLevel = document.querySelector('.upload-effect-level');
-    var effectLevelPin = document.querySelector('.upload-effect-level-pin');
-    var effectLevelVal = document.querySelector('.upload-effect-level-val');
-
-    var onEffectControlsClick = function (evt) {
-      var target = evt.target;
-      if (target.type === 'radio') {
-        isImageHasFilter(target);
-        resetEffectValue();
-        callback.style = '';
-        callback.classList = '';
-        callback.classList.add('effect-' + target.value);
-      }
-    };
-
-    var resetEffectValue = function () {
-      effectLevelPin.style.left = '100%';
-      effectLevelVal.style.width = '100%';
-    };
-
-    var isImageHasFilter = function (target) {
-      if (target.value !== 'none') {
-        return uploadEffectLevel.classList.remove('hidden');
-      } else {
-        return uploadEffectLevel.classList.add('hidden');
-      }
-    };
-
-    filterElement.addEventListener('click', onEffectControlsClick);
+    var oldFilter = 'none';
+    var newFilter;
+    filterElement.addEventListener('change', function (evt) {
+      newFilter = evt.target.value;
+      callback(newFilter, oldFilter);
+      oldFilter = evt.target.value;
+    });
   };
 })();
